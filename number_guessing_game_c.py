72c4ebcf83b3817e14743518866dbd325b675f3d
joshgoshbgosh <69131589+joshgoshbgosh@users.noreply.github.com>
2020-10-06 13:17:43 -0400

    numberGameCcomplete

--- number_guessing_game_c.py
+++ number_guessing_game_c.py
@@ -1,18 +1,16 @@
 import random
 high_num = 100
 low_num = 1
+computerGuess = 50
 
 
 mynumber = int(input("Pick a number between 1 and 100: "))
 tries = 1
-computerGuess = random.randint(low_num, high_num)
+
 
 while computerGuess != mynumber:
-    computerGuess = random.randint(low_num, high_num)
-    if tries > 5:
-        print("Game Over!")
-        break
-    elif computerGuess > mynumber:
+    computerGuess = (low_num + high_num)//2
+    if computerGuess > mynumber:
         print("Sorry to high!")
         high_num = computerGuess
         print(computerGuess)
@@ -24,5 +22,4 @@ while computerGuess != mynumber:
 
     tries +=1
 
-else:
-    print("You did it! It only took you", tries, " tries!")
+print("You did it! It only took you", tries, " tries!")
