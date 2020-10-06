9a6cce488834a37fe83df823fcbe91df5d95a767
joshgoshbgosh <69131589+joshgoshbgosh@users.noreply.github.com>
2020-10-06 09:16:14 -0400

    changed 1 note

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -2,16 +2,16 @@
 
 import random
 
-print ("Do you want to play a game??? You will only survive if you choose wisely. You only have 5 tries.  Good Luck!")
+print ("Do you want to play a game??? You will only survive if you choose wisely. \n You only have 5 tries.  Good Luck!")
 
-randomNumber = random.randint(1, 99)  #randint selects a random number from the specified numbers 1-99.
+randomNumber = random.randint(1, 100)  #randint selects a random number from the specified numbers 1-99.
 
-guess = int(input("enter a number between 1 and 99: "))
+guess = int(input("enter a number between 1 and 100: "))
 tries = 1
 
 while (guess != randomNumber):
-    if tries > 5:
-        print("You did not choose wisely!")
+    if tries > 5:        #number of tries available to beat the game
+        print("You did not choose wisely! It was", randomNumber)
         break
     elif guess < randomNumber:
         print ("To Low")
@@ -19,7 +19,7 @@ while (guess != randomNumber):
         print ("To High")
 
     guess = int(input("Guess Again:"))
-    tries +=1
+    tries +=1   #this is counting tries being used.
 
 else:
     print ("You are wise!")
