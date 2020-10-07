4cd853c6bbe741721367f1c89be18e2732d40e83
joshgoshbgosh <69131589+joshgoshbgosh@users.noreply.github.com>
2020-10-06 22:53:39 -0400

    hangmancomplete

--- hangman.py
+++ hangman.py
@@ -1,5 +1,39 @@
 import random
 with open ('/usr/share/dict/words') as infile:
     words = infile.read().split()
-random_word = random.choice(words)
-print(random_word)
+random_word = random.choice(words).lower().replace("\n", "")
+
+used_characters = []
+guess_word = []   #setting up my empty list for letters and word
+len_word = len(random_word) #assigning length and random_word to len_word
+
+def display():
+    for char in random_word:
+        guess_word.append('_') #this seperates the word to look like this - - - - - - - - -
+    print("The word has ", len_word, "letters.")
+    return guess_word
+
+def play():
+    turns = 20
+    while turns > 0:      #creating amount of tries and keeping up with amount left
+        print(turns, "guesses left")
+        print(guess_word)
+        if '_' not in guess_word:
+            print("You got it!")
+            break
+
+        letter_guessed = (input("Guess a letter >")).lower()
+        if letter_guessed in used_characters:
+            print ("Already picked this one...")
+        else:
+            turns -= 1      #here is saying if a letter is already picked  to notify and take away tries.
+        if turns >= 1:
+            print ("Sorry, not correct!")
+        if turns == 0:
+            print("Game Over!")
+            print("Word was", random_word)
+        for letter in range(0, len_word):
+            if random_word[letter] == letter_guessed:
+                guess_word[letter] = letter_guessed
+display()
+play()
